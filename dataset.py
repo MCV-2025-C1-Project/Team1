ba620f4418b21744f8dffd80a46cac8680cb083f
adriangt2001 <adriangt2001@hotmail.com>
2025-10-01 22:19:46 +0200

    Created dataset class

---
+++ dataset.py
@@ -0,0 +1,53 @@
+import time
+
+import os
+import glob
+
+import cv2
+import numpy as np
+
+import readability
+
+class Dataset:
+    """Class for the query dataset."""
+    def __init__(self, path: str, color_space: readability.COLOR_SPACES='rgb'):
+        self.color_space = color_space
+        self.__load_dataset(path)
+    
+    def __load_dataset(self, data_path: str):
+        """Load the query dataset."""
+        self.images = []
+        root_dir = os.path.abspath(os.path.expanduser(data_path))
+        pattern = os.path.join(root_dir, '*.jpg')
+        for image_path in glob.iglob(pattern, root_dir=root_dir):
+            self.images.append(self.__load_img(image_path))
+
+    def __load_img(self, img_path: str):
+        """Loads an image."""
+        if self.color_space == 'rgb':
+            cv2_cvt_code = cv2.COLOR_BGR2RGB
+        elif self.color_space == 'hsv':
+            cv2_cvt_code = cv2.COLOR_BGR2HSV
+        
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2_cvt_code)
+        return image 
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx: int):
+        return self.images[idx]
+
+if __name__ == '__main__':
+    rel_path = '../Datasets/qsd1_w1'
+    db = Dataset(rel_path, color_space='rgb')
+    print(f'Dataset length: {len(db)}')
+
+    abs_path = '/home/adriangt2001/MCVC/C1/Project/Datasets/qsd1_w1'
+    db = Dataset(abs_path, color_space='hsv')
+    print(f'Dataset length: {len(db)}')
+    
+    compr_abs_path = '~/MCVC/C1/Project/Datasets/qsd1_w1'
+    db = Dataset(compr_abs_path, color_space='hsv')
+    print(f'Dataset length: {len(db)}')
